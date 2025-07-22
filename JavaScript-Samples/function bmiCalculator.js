3ab5cbcf645b10b30fbb1e1db55c863ada99bca1
MR POOL <71345322+vishnu-madathodi@users.noreply.github.com>
2025-07-22 23:35:19 +0530

    Add files via upload

---
+++ JavaScript-Samples/function bmiCalculator.js
@@ -0,0 +1,16 @@
+function bmiCalculator (weight, height) {
+    var interpretation;
+    let bmi = weight / (Math.pow(height,2));
+    
+    if(bmi < 18.5){
+        interpretation = `Your BMI is ${bmi}, so you are underweight.`;
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9){
+        interpretation = `Your BMI is ${bmi}, so you have a normal weight.`;
+    }
+    else if(bmi > 24.9){
+        interpretation = `Your BMI is ${bmi}, so you are overweight.`
+    }
+    
+    return interpretation;
+}
