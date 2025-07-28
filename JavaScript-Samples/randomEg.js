edf93b3e2decd178111284a1497b7804861cf18f
MR POOL <71345322+vishnu-madathodi@users.noreply.github.com>
2025-07-29 01:20:55 +0530

    Create randomEg.js

---
+++ JavaScript-Samples/randomEg.js
@@ -0,0 +1,13 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    
+    let length = names.length;
+    let rndCount = Math.floor(Math.random() * length);
+    return `${names[rndCount]} is going to buy lunch today!`;
+    
+
+/******Don't change the code below*******/    
+}
