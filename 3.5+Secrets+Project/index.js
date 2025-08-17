10673369e4e21714f1a49ccf01c9660929f62100
MR POOL <71345322+vishnu-madathodi@users.noreply.github.com>
2025-08-18 00:27:34 +0530

    Add files via upload

---
+++ 3.5+Secrets+Project/index.js
@@ -0,0 +1,45 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var isAuthorized = false;
+
+//middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(checkPassword);
+
+//middleware create for checking password
+function checkPassword(req, res, next){
+    if(req.body["password"] === "ILoveProgramming"){
+        isAuthorized = true;
+    }
+    next();
+}
+
+//handlers
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.post("/check", (req, res) => {
+    if(isAuthorized){
+        res.sendFile(`${__dirname}/public/secret.html`);
+    }
+    else{
+        res.redirect("/");
+    }
+    isAuthorized = false;
+});
+
+app.listen(port, () => {
+    console.log(`App listening in the port ${port}`);
+});
